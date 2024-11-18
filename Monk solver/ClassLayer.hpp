90a7f2a5b9522ee93bc9dbfe1cfe4a4c2058c935
FraLiturri <francesco.liturri.dev@gmail.com>
2024-11-18 16:23:14 +0100

    Code improvement

--- Monk solver/ClassLayer.hpp
+++ Monk solver/ClassLayer.hpp
@@ -1,139 +1,61 @@
 #include <iostream>
 #include <vector>
 
-const int hidden_number;
+const int hidden_number = 3;
 const int inputsize;
 const int outputsize;
-std::vector<int, hidden_number> LayerSizes = {}
 
-class Layer {
+std::vector<int> LayerSizes(hidden_number); 
+
+class Layer
+{
 public:
-    virtual ~Layer() = default;
-    virtual int NumNode() const = 0;
-    virtual Layer* GetPrevLayer() const = 0;
+	virtual ~Layer() = default;
+	virtual int NumNode() const = 0;
+	virtual Layer *GetPrevLayer() const = 0;
 };
 
-template<class T = Layer>
-class HiddenLayer : public Layer {
+template <class T = Layer>
+class HiddenLayer : public Layer
+{
 public:
-    HiddenLayer() : NumNodes_1(0), PrevLayer(nullptr) {}
-    HiddenLayer(int NumNodes, T* LayerPrecedente) 
-        : NumNodes_1(NumNodes), PrevLayer(LayerPrecedente) {}
+	HiddenLayer() : NumNodes_1(0), PrevLayer(nullptr) {}
+	HiddenLayer(int NumNodes, T *LayerPrecedente)
+		: NumNodes_1(NumNodes), PrevLayer(LayerPrecedente) {}
 
-    int NumNode() const override { return NumNodes_1; }
+	int NumNode() const override { return NumNodes_1; }
 
-    Layer* GetPrevLayer() const override { return PrevLayer; }
+	Layer *GetPrevLayer() const override { return PrevLayer; }
 
 protected:
-    T* PrevLayer;
+	T *PrevLayer;
 
 private:
-    int NumNodes_1;
+	int NumNodes_1;
 };
-
-
 
 // InputLayer derivato da Layer
-class InputLayer : public Layer {
+class InputLayer : public Layer
+{
 public:
-    InputLayer() : NumNodes_1(0) {}
-    InputLayer(int NumNodes) : NumNodes_1(NumNodes) {}
+	InputLayer() : NumNodes_1(0) {}
+	InputLayer(int NumNodes) : NumNodes_1(NumNodes) {}
 
-    int NumNode() const override { return NumNodes_1; }
-    Layer* GetPrevLayer() const override { return nullptr; }
+	int NumNode() const override { return NumNodes_1; }
+	Layer *GetPrevLayer() const override { return nullptr; }
 
 private:
-    int NumNodes_1 = inputsize;
+	int NumNodes_1 = inputsize;
 };
 
-
-
-
-
-class MLP 
-{
-	public:	
-		MLP(){}
-		MLP(InputLayer *I, Layer *O):
-			OutputLayer(O), InLayer(I);
-	
-	
-	private:
-		//std::vector<Layer*> LayerPointers;
-		Layer *OutputLayer;
-		Layer *InLayer;
-	
-	
-}
-
-
-
-/*
-template<class T>
 class MLP
 {
-	public:
-		MLP(){}
-		MLP(InputLayer *a, Layer *Output, T *LayerPointer):
-					LayerInput(a), LayerOutput(Output)
-					{
-						
-						LayerPointer = 
-						while(LayerPointer != NULL)
-						{
-							LayersPointer.push_back();
-						}
-						
-						
-						
-						
-					}
+public:
+	MLP() {}
+	MLP(InputLayer *I, Layer *O) : OutputLayer(O), InLayer(I) {};
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-	private:
-		std::vector<Layer*> LayersPointer;
-		InputLayer *LayerInput;
-		Layer *LayerOutput;
-}
-
-
-
-
-
-
-MLP sempronio(3, 2, [3.4.5], )
-
-
-
-
-/*main
-
-
-std::vector<int> Dati;
-
-InputLayer *a = new InputLayer(std::size(Dati));
-Layer *b = new Layer(16, a);
-Layer *c = new Layer(16, b);
-Layer *output = new Layer(2, c);
-
-MLP *Modello = new Modello()
-
-
-Modello.train();
-
-Modello.
-* 
-*/
+private:
+	// std::vector<Layer*> LayerPointers;
+	Layer *OutputLayer;
+	Layer *InLayer;
+};
